cdc33d2da29f44dfb1caff619e95ba9e0f495ad9
Leonel Bazan <leo.bazan.x@gmail.com>
2020-05-02 01:03:41 -0300

    WIP [Gameboy] [Test] Hay que hacer los test del parcer

---
+++ gameboy/test/test_parcer.c
@@ -0,0 +1,4 @@
+#include "test_parcer.h"
+
+
+
---
+++ gameboy/test/test_parcer.h
@@ -0,0 +1,7 @@
+#ifndef TEST_TEST_PARCER_H_
+#define TEST_TEST_PARCER_H_
+
+#include "CUnit/Basic.h"
+#include "../gameboy.h"
+
+#endif /* TEST_TEST_PARCER_H_ */
