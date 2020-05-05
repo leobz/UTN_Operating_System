4c74177ae5f02cb46ef02514071e306f9475fee0
Leonel Bazan <leo.bazan.x@gmail.com>
2020-05-05 15:45:50 -0300

    [Team] Agrego funcion pasar_a_ready(TCB)
    Pasa el TCB a la lista global 'ready' y cambia el estado
    del tcb a READY

--- team/main.c
+++ team/main.c
@@ -1,18 +1,30 @@
 #include "main.h"
+#include "test/testing.h"
 
 t_list* ready;
 t_list* new;
 t_list* blocked;
 
-int main(){
+int main(int argc, char ** argv) {
 	inicializar_listas();
 
+	if (argc == 2) {
+		if (strcmp(argv[1], "test") == 0){
+			correrTests();
+		}
+	}
 
-    return 0;
+	return 0;
 }
 
-void inicializar_listas(){
+void inicializar_listas() {
 	ready = list_create();
 	blocked = list_create();
 	new = list_create();
+}
+
+void pasar_a_ready(tcb_entrenador* tcb) {
+	// TODO: implementar ordenamiento por cercania
+	list_add(ready, tcb);
+	tcb->estado = READY;
 }
--- team/main.h
+++ team/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <commons/collections/list.h>
 #include <pthread.h>
+
 
 // ESTRUCTURAS
 typedef enum {
@@ -24,10 +28,11 @@ typedef struct {
 	int tid;
 	tcb_estado estado;
 	t_posicion posicion;
+	// TODO: Agregar pokemons objetivo
 } tcb_entrenador;
 
 
 
 void inicializar_listas();
-
+void pasar_a_ready(tcb_entrenador* tcb);
 #endif /* MAIN_H_ */
