72f02bd5cdab8a7c938aba546c0a81209a47b5f5
Leonel Bazan <leo.bazan.x@gmail.com>
2020-05-11 11:44:41 -0300

    [FIX-CONFLICTOS] [BROKER] Elimino todos los cambios del Broker para que este como DEV
    La razon es que el codigo del broker esta interfiriendo con los demas modulos.
    Esto no deberia pasar, los modulos a excepcion de Utils deben ser INDEPENDIENTES

--- broker/main.c
+++ broker/main.c
@@ -1,13 +1,3 @@
-#include "broker.h"
-#include "server.h"
-
 int main(){
-
-	int socket_servidor = iniciar_servidor(IP, PUERTO);
-
-    while(1){
-    	esperar_cliente(socket_servidor, &procesar_mensaje_recibido);}
-
-
 	return 0;
 }
--- broker/test/test_new_pokemon.c
+++
@@ -1,9 +0,0 @@
-/*
- * test_new_pokemon.c
- *
- *  Created on: May 10, 2020
- *      Author: diego
- */
-
-
-
