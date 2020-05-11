f7fedf8c46c6f3792314edb4c655bdd39fcd19f0
Leonel Bazan <leo.bazan.x@gmail.com>
2020-05-11 14:49:02 -0300

    [Utils] Agrego manejo de Error para lectura de Config y creacion de logger

--- utils/config.h
+++ utils/config.h
@@ -1,7 +1,9 @@
 #ifndef CONFIGURATION_H_
 #define CONFIGURATION_H_
 
-#include<commons/config.h>
+#include <commons/config.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 t_config* leer_config(char* path_archivo);
 void destruir_config(t_config* config);
