1326f674f669b7eaeb716c5f39fc58f598a9e774
Leonel Bazan <leo.bazan.x@gmail.com>
2020-05-11 14:49:02 -0300

    [Utils] Agrego manejo de Error para lectura de Config y creacion de logger

--- utils/config.c
+++ utils/config.c
@@ -1,11 +1,17 @@
 #include "config.h"
 
-t_config* leer_config(char* path_archivo)
-{
-	return config_create(path_archivo);
+t_config* leer_config(char* path_archivo) {
+	t_config* config;
+	if ((config = config_create(path_archivo)) == NULL) {
+		printf(
+				"ERROR: Archivo de configuracion '%s' no existe o es invalido\n",
+				path_archivo);
+		exit(1);
+	}
+
+	return config;
 }
 
-void destruir_config(t_config* config)
-{
+void destruir_config(t_config* config) {
 	config_destroy(config);
 }
--- utils/log.c
+++ utils/log.c
@@ -1,11 +1,18 @@
 #include "log.h"
 
-t_log* iniciar_logger(char* nombre_archivo, char* nombre_programa, t_log_level nivel_detalle)
-{
-	return log_create(nombre_archivo, nombre_programa, 0, nivel_detalle);
+t_log* iniciar_logger(char* nombre_archivo, char* nombre_programa,
+		t_log_level nivel_detalle) {
+	t_log* logger;
+
+	if ((logger = log_create(nombre_archivo, nombre_programa, 0, nivel_detalle))
+			== NULL) {
+		printf("\nERROR: No se pudo crear Logger\n");
+		exit(1);
+	}
+
+	return logger;
 }
 
-void destruir_logger(t_log* logger)
-{
+void destruir_logger(t_log* logger) {
 	log_destroy(logger);
 }
