16c161d695ad22fbed9618f14006bcdaf8c1b1a7
Ezequiel Gauna <gaunaezequiel.a@gmail.com>
2020-05-31 11:34:56 -0300

    Corrige formato

--- utils/serializacion.h
+++ utils/serializacion.h
@@ -7,20 +7,23 @@
 
 // ESTRUCTURAS
 
-
 typedef enum {
 	OP_ERROR = -1,
 	// Mensaje sin correlativo t_mensaje_sc
-	NEW_POKEMON = 0, GET_POKEMON = 1, CATCH_POKEMON = 2,
+	NEW_POKEMON = 0,
+	GET_POKEMON = 1,
+	CATCH_POKEMON = 2,
 	// Mensaje con correlativo t_mensaje_cc
-	APPEARED_POKEMON = 3, LOCALIZED_POKEMON = 4, CAUGHT_POKEMON = 5,
+	APPEARED_POKEMON = 3,
+	LOCALIZED_POKEMON = 4,
+	CAUGHT_POKEMON = 5,
 	SUSCRIPCION = 6
-	// TODO: Definir codigos de operacion restantes
+// TODO: Definir codigos de operacion restantes
 } op_code;
 
-typedef enum{
-	FAIL=0, OK=1
-}estado;
+typedef enum {
+	FAIL = 0, OK = 1
+} estado;
 
 typedef struct {
 	int size;
@@ -33,7 +36,6 @@ typedef struct {
 	int id_correlativo;
 } t_paquete;
 
-
 typedef struct {
 	int length_pokemon;
 	char* pokemon;
@@ -41,40 +43,42 @@ typedef struct {
 	int posy;
 } t_mensaje_appeared;
 
-
-
 typedef struct {
 	int length_pokemon;
 	char* pokemon;
 	int posx;
 	int posy;
 	int cantidad;
-}t_mensaje_new;
+} t_mensaje_new;
 
 // PAQUETES GENERAL
-t_paquete* crear_paquete(int codigo_operacion, t_buffer* buffer, int id_correlativo);
+t_paquete* crear_paquete(int codigo_operacion, t_buffer* buffer,
+		int id_correlativo);
 void eliminar_paquete(t_paquete* paquete);
 void* serializar_paquete(t_paquete* paquete, int bytes);
-
-
 
 // BUFFERS GENERAL
 void* deserializar_buffer_de_un_string(t_buffer* buffer);
 
 // NEW_POKEMON
-void* serializar_new_pokemon(int* bytes, char* nombre_pokemon, int pos_x,int pos_y,int cantidad, int id_correlativo);
-t_buffer* buffer_new_pokemon(char* nombre_pokemon, int pos_x, int pos_y,int cantidad);
+void* serializar_new_pokemon(int* bytes, char* nombre_pokemon, int pos_x,
+		int pos_y, int cantidad, int id_correlativo);
+t_buffer* buffer_new_pokemon(char* nombre_pokemon, int pos_x, int pos_y,
+		int cantidad);
 
 // GET_POKEMON
-void* serializar_get_pokemon(int* bytes, char* nombre_pokemon, int id_correlativo);
+void* serializar_get_pokemon(int* bytes, char* nombre_pokemon,
+		int id_correlativo);
 t_buffer* buffer_get_pokemon(char* nombre_pokemon);
 
 //CATCH_POKEMON
-void* serializar_catch_pokemon(int* bytes, char* nombre_pokemon, int pos_x, int pos_y, int id_correlativo);
+void* serializar_catch_pokemon(int* bytes, char* nombre_pokemon, int pos_x,
+		int pos_y, int id_correlativo);
 t_buffer* buffer_catch_pokemon(char* nombre_pokemon, int pos_x, int pos_y);
 
 // APPEARED_POKEMON
-void* serializar_appeared_pokemon(int* bytes, char* nombre_pokemon, int pos_x, int pos_y, int id_correlativo);
+void* serializar_appeared_pokemon(int* bytes, char* nombre_pokemon, int pos_x,
+		int pos_y, int id_correlativo);
 t_buffer* buffer_appeared_pokemon(char* nombre_pokemon, int pos_x, int pos_y);
 t_mensaje_appeared* get_mensaje_appeared_by_buffer(t_buffer* buffer);
 void eliminar_mensaje_appeared(t_mensaje_appeared* mensaje_appeared);
