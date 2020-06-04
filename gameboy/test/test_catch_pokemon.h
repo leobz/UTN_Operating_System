fa9b601b0b12600ec9c917e66e085281597aa222
Ezequiel Gauna <gaunaezequiel.a@gmail.com>
2020-06-04 01:04:32 -0300

    Agrego archivos faltantes

---
+++ gameboy/test/test_catch_pokemon.h
@@ -0,0 +1,15 @@
+/*
+ * test_catch_pokemon.h
+ *
+ *  Created on: 3 jun. 2020
+ *      Author: utnso
+ */
+
+#ifndef TEST_TEST_CATCH_POKEMON_H_
+#define TEST_TEST_CATCH_POKEMON_H_
+
+#include "test_utils.h"
+
+void empaquetar_y_desempaquetar_catch_pokemon();
+
+#endif /* TEST_TEST_CATCH_POKEMON_H_ */
---
+++ gameboy/test/test_utils.h
@@ -0,0 +1,14 @@
+/*
+ * test_utils.h
+ *
+ *  Created on: 4 jun. 2020
+ *      Author: utnso
+ */
+
+#ifndef TEST_TEST_UTILS_H_
+#define TEST_TEST_UTILS_H_
+
+#include "CUnit/Basic.h"
+#include "../gameboy.h"
+
+#endif /* TEST_TEST_UTILS_H_ */
