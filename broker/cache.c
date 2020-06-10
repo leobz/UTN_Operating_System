31072281f7319eda448ad162fdacd3083b8f74ba
ezequiel-laime <ezequiellaime96@gmail.com>
2020-06-09 23:08:00 -0300

    [BS] Agregado de funciones para hojas libres - Agregado de funcion de obtencion de hojas libres - Agregado de funcion de ordenamiento de hojas libres segun algoritmo de particion libre

--- broker/cache.c
+++ broker/cache.c
@@ -2,6 +2,7 @@
 
 void inicializar_memoria_cache() {
 	memoria_cache = malloc(broker_config->tamanio_memoria);
+	particion_bs = malloc(sizeof(t_particion_bs));
 
 	inicializar_mutex_cache();
 }
@@ -18,10 +19,45 @@ void inicializar_mutex_cache() {
 
 void agregar_mensaje_memoria_cache(t_mensaje* mensaje) {
 	if (strcmp(broker_config->algoritmo_memoria, "BS") == 0) {
-		// logica de buddy system
+		t_list* hojas_libres = list_create();
+
+		obtener_hojas_libres_bs(hojas_libres, particion_bs);
+		ordenar_hojas_libres_segun_algoritmo_particion_libre(hojas_libres);
 	}
 	else if (strcmp(broker_config->algoritmo_memoria, "PARTICIONES") == 0) {
 			// logica de particionamiento dinamico
+	}
+}
+
+void obtener_hojas_libre_bs(t_list* hojas_libres, t_particion_bs* particion) {
+
+	if ((particion->primer_hijo != NULL || particion->segundo_hijo != NULL) && particion->esta_libre) {
+		if (particion->primer_hijo != NULL)
+			obtener_hojas_libre_bs(hojas_libres, particion->primer_hijo);
+
+		if (particion->segundo_hijo != NULL)
+			obtener_hojas_libre_bs(hojas_libres, particion->segundo_hijo);
+	}
+	else if (particion->esta_libre) {
+		list_add(hojas_libres, particion);
+	}
+}
+
+bool es_menor_offset(t_particion_bs* hoja_libre, t_particion_bs* siguiente_hoja_libre) {
+	return hoja_libre->offset < siguiente_hoja_libre->offset;
+}
+
+bool es_menor_tamanio(t_particion_bs* hoja_libre, t_particion_bs* siguiente_hoja_libre) {
+	return hoja_libre->tamanio_particion < siguiente_hoja_libre->tamanio_particion;
+}
+
+void ordenar_hojas_libres_segun_algoritmo_particion_libre(t_list* hojas_libres) {
+
+	if (strcmp(broker_config->algoritmo_particion_libre, "FF") == 0) {
+		list_sort(hojas_libres, es_menor_offset);
+	}
+	else if (strcmp(broker_config->algoritmo_particion_libre, "BF") == 0) {
+		list_sort(hojas_libres, es_menor_tamanio);
 	}
 }
 
@@ -33,5 +69,6 @@ void finalizar_mutex_cache() {
 void finalizar_memoria_cache() {
 	finalizar_mutex_cache();
 
+	free(particion_bs);
 	free(memoria_cache);
 }
--- broker/cache.h
+++ broker/cache.h
@@ -5,15 +5,16 @@
 
 pthread_mutex_t m_cache[6];
 
+t_particion_bs* particion_bs;
 void* memoria_cache;
 
-typedef struct {
+typedef struct particion_bs {
 	int tamanio_particion;
-	bool esta_ocupado;
+	bool esta_libre;
 	int offset;
 	int size_mensaje;
-	struct t_particion_bs* primer_hijo;
-	struct t_particion_bs* segundo_hijo;
+	struct particion_bs* primer_hijo;
+	struct particion_bs* segundo_hijo;
 } t_particion_bs;
 
 
@@ -23,10 +24,13 @@ void inicializar_mutex_cache();
 
 // Funciones cache
 void agregar_mensaje_memoria_cache(t_mensaje* mensaje);
+void obtener_hojas_libre_bs(t_list* hojas_libres, t_particion_bs* particion);
+bool es_menor_offset(t_particion_bs* hoja_libre, t_particion_bs* siguiente_hoja_libre);
+bool es_menor_tamanio(t_particion_bs* hoja_libre, t_particion_bs* siguiente_hoja_libre);
+void ordenar_hojas_libres_segun_algoritmo_particion_libre(t_list* hojas_libres);
 
 // Finalización cache
 void finalizar_mutex_cache();
 void finalizar_memoria_cache();
-
 
 #endif /* CACHE_H_ */
