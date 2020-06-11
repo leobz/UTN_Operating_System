4ec3ff8f8cc1efae4d95aef0b75a86af8d32dfe7
olsztajn <tamaraolsztajn@gmail.com>
2020-06-11 00:27:27 -0300

    TODO:NEW POKEMON

--- game-card/main.c
+++ game-card/main.c
@@ -1,3 +1,29 @@
 int main(){
+	//NEW POKEMON
+	//TODO: Listado de pokemons, recorrer el objetivo general para ver los distintos pokemons que hay que tener
+	/*TODO:Si el pokemon no existe:
+	 1) crear directorio metada.bin (ver txt.c)
+	 2) Buscar bloque libre en bitmap-> recorrer buscando un 0, ese es el bloque libre --> mirar bitarray.c
+	 3) completar el metadata.bin
+	 */
+	/*TODO: Si el pokemon existe:
+	if(esta abierto) --> chequear Y/N
+		Busco el tiempo en archivo de configuracion
+	else
+		fopen archivo y unirlos todos
+	*/
+
+	/*TODO: If(Existe la posicion){cantidad+lo que sumo}
+	  	  	else{agrego una linea al final del archivo}
+	  	  	 sleep()
+	 	 	 fclose()
+	 */
+	/*TODO:
+	 	 Conecto Gamecard con broker
+	 	 recibo la respuesta
+	 	 en caso que hubo respuesta mando caught
+	 	 si no hubo respuesta logueo el error
+	 */
+
     return 0;
 }
