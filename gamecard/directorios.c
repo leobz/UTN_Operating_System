a9455a6e8dccdac473ba27ebe4e0c5458c301657
olsztajn <tamaraolsztajn@gmail.com>
2020-06-14 17:57:37 -0300

    creo archivos bitmap.bin y metadata.bin

--- gamecard/directorios.c
+++ gamecard/directorios.c
@@ -15,14 +15,24 @@ void inicializar_directorios(t_gamecard_config* gamecard_config){
 	string_append(&creacion_path_directorio,gamecard_config->punto_montaje_tallgrass);
 	string_append(&creacion_path_directorio,"/Files");
 	mkdir(creacion_path_directorio,0777);
+	creacion_path_directorio=NULL;
 
 	//Creo Blocks
-	creacion_path_directorio=NULL;
 	creacion_path_directorio = string_new();
 	string_append(&creacion_path_directorio,gamecard_config->punto_montaje_tallgrass);
 	string_append(&creacion_path_directorio,"/Blocks");
 	mkdir(creacion_path_directorio,0777);
+	creacion_path_directorio=NULL;
 	free(creacion_path_directorio);
-		creacion_path_directorio=NULL;
+
+	//Creo archivo Bitmap.bin
+		FILE *bitmap;
+		bitmap=fopen("/home/utnso/tall-grass/Metadata/Bitmap.bin","w+");
+		fclose(bitmap);
+
+	//Creo archivo Metadata.bin
+		FILE *metadata;
+		metadata=fopen("/home/utnso/tall-grass/Metadata/Metadata.bin","w+");
+		fclose(metadata);
 
 }
