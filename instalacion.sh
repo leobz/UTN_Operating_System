f8e095c17a4c733297332a934361956a9afc4615
Leonel Bazan <leo.bazan.x@gmail.com>
2020-07-17 17:07:24 -0300

    Deploy: Creo script backuppear_makefiles.sh

--- instalacion.sh
+++ instalacion.sh
@@ -1,21 +1,22 @@
+echo Actualizando Makefiles
 python3 actualizar_makefile.py
 
 echo
-echo Compilo Utils
+echo Compilando Utils
 make clean -C utils/Debug 2>> error.log  1>> error.log
 make -C utils/Debug 2>> error.log  1>> error.log
 
 echo
-echo Compilo Gameboy
+echo Compilando Gameboy
 make clean -C gameboy/Debug 2>> error.log  1>> error.log
 make -C gameboy/Debug 2>> error.log  1>> error.log
 
 echo
-echo Compilo Broker
+echo Compilando Broker
 make clean -C broker/Debug 2>> error.log  1>> error.log
 make -C broker/Debug 2>> error.log  1>> error.log
 
 echo
-echo Compilo Team
+echo Compilando Team
 make -C team/Debug 2>> error.log  1>> error.log
 make -C team/Debug 2>> error.log  1>> error.log
