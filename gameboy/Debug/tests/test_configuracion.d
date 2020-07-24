5a200186b009ea6347aeb66e64d283e150cd5f7a
Leonel Bazan <leo.bazan.x@gmail.com>
2020-07-24 12:08:30 -0300

    Reseteo todos los .gitignore

--- gameboy/Debug/tests/test_configuracion.d
+++
@@ -1,9 +0,0 @@
-tests/test_configuracion.o: ../tests/test_configuracion.c \
- ../tests/test_configuracion.h ../tests/../gameboy.h \
- ../tests/../../utils/config.h
-
-../tests/test_configuracion.h:
-
-../tests/../gameboy.h:
-
-../tests/../../utils/config.h:
--- gameboy/Debug/tests/testing.d
+++
@@ -1,8 +0,0 @@
-tests/testing.o: ../tests/testing.c ../tests/test_configuracion.h \
- ../tests/../gameboy.h ../tests/../../utils/config.h
-
-../tests/test_configuracion.h:
-
-../tests/../gameboy.h:
-
-../tests/../../utils/config.h:
