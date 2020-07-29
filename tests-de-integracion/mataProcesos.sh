c2a3137818af56f1b71e62901df7a462be132458
Leonel Bazan <leo.bazan.x@gmail.com>
2020-07-28 22:22:25 -0300

    Script Gamecard Memory Leaks

--- tests-de-integracion/mataProcesos.sh
+++ tests-de-integracion/mataProcesos.sh
@@ -3,3 +3,4 @@ kill `ps -ef|grep -v grep |grep team| awk '{print $2}'`
 kill `ps -ef|grep -v grep |grep broker| awk '{print $2}'`
 kill `ps -ef|grep -v grep |grep gameboy| awk '{print $2}'`
 kill `ps -ef|grep -v grep |grep gamecard| awk '{print $2}'`
+kill `ps -ef|grep -v grep |grep valgrind| awk '{print $2}'`
