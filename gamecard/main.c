ce4e14031ce7630533e303165d9a219cbda88d22
Leonel Bazan <Leonel Bazan>
2020-08-03 03:14:01 -0300

    Gamecard: Reintento de conexion

--- gamecard/main.c
+++ gamecard/main.c
@@ -1,12 +1,34 @@
 #include "main.h"
 
+t_datos_suscripcion* crear_datos_suscripcion(int id_proceso, int cola) {
+	t_datos_suscripcion* datos_suscripcion = malloc(sizeof(t_datos_suscripcion));
+	datos_suscripcion->conexion = crear_conexion(gamecard_config->ip_broker, gamecard_config->puerto_broker);
+	datos_suscripcion->cola = cola;
+	datos_suscripcion->id_proceso = id_proceso * 10 + datos_suscripcion->cola;
+	return datos_suscripcion;
+}
+
 int main(){
 
 	inicializar_gamecard();
 	inicializar_directorios();
 	inicializar_diccionarios();
 	inicializar_semaforos();
-	pthread_create(&suscripcion_broker, NULL, (void*)suscribirme_al_broker, NULL);
+
+	int id_proceso = (int)getpid();
+
+	char* puerto_broker = gamecard_config->puerto_broker;
+	char* ip_broker = gamecard_config->ip_broker;
+
+
+	t_datos_suscripcion* datos_suscripcion_get = crear_datos_suscripcion(id_proceso, GET_POKEMON);
+	t_datos_suscripcion* datos_suscripcion_new = crear_datos_suscripcion(id_proceso, NEW_POKEMON);
+	t_datos_suscripcion* datos_suscripcion_catch = crear_datos_suscripcion(id_proceso, CATCH_POKEMON);
+
+	iniciar_hilo_suscripcion(datos_suscripcion_get);
+	iniciar_hilo_suscripcion(datos_suscripcion_new);
+	iniciar_hilo_suscripcion(datos_suscripcion_catch);
+
 
 
 	// Creo la conexion para escuchar
@@ -18,7 +40,6 @@ int main(){
 		esperar_cliente(socket_servidor, &procesar_mensaje_recibido_cliente);
 	}
 
-	pthread_detach(suscripcion_broker);
 
 	finalizar_gamecard();
 }
--- gamecard/servidor_gamecard.h
+++ gamecard/servidor_gamecard.h
@@ -9,13 +9,21 @@
 #define SERVIDOR_GAMECARD_H_
 
 #include "directorios.h"
+#include "../utils/serializacion.h"
 
 int id_proceso_gamecard;
+int finalizo_suscripcion;
+
+typedef struct {
+	int id_proceso;
+	int conexion;
+	int cola;
+} t_datos_suscripcion;
 
 // Funciones
 void procesar_mensaje_recibido_broker(t_paquete_socket* paquete_socket);
 void procesar_mensaje_recibido_cliente(t_paquete_socket* paquete_socket);
-void servidor_gamecard(int conexion);
+void recibir_mensajes_gamecard(t_datos_suscripcion* datos_suscripcion);
 void enviar_confirmacion(int id, int confirmacion, int socket);
 void preparar_confirmacion(int id_men);
 void preparar_confirmacion(int id_men);
