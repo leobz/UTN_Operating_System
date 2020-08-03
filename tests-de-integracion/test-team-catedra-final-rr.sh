755bb9f36d366b4a433fca62874b4c4f28d90198
Leonel Bazan <leo.bazan.x@gmail.com>
2020-08-03 17:35:44 -0300

    Unifico tests Broker
    test-broker-catedra-base-tamanio-minimo-16.sh

--- tests-de-integracion/test-team-catedra-final-rr.sh
+++ tests-de-integracion/test-team-catedra-final-rr.sh
@@ -32,4 +32,4 @@ byexample -l shell team-catedra-final-rr.md
 # Restaurando configuracion inicial
 rm team.config; cp team.config.auxiliar team.config; rm team.config.auxiliar
 
-1>/dev/null 2>/dev/nul         sh mataProcesos.sh
+1>/dev/null 2>/dev/null         sh mataProcesos.sh
